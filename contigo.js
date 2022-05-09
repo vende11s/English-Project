742c64656c963c9e736b1e66446a9e9b717769b0
vende11s <wie.rzbinski9@gmail.com>
2022-05-09 17:51:52 +0200

    upt

--- contigo.js
+++ contigo.js
@@ -1,5 +1,42 @@
-fetch('database.txt')
-  .then(response => response.text())
-  .then(text => console.log(text))
+/*async function downloadFile(file) {
+	let response = await fetch(file);
+		
+	if(response.status != 200) {
+		throw new Error("Server Error");
+	}
+		
+	// read response stream as text
+	let text_data = await response.text();
 
-//document.getElementById("Unit-choose").innerHTML = fileContent;
+	return text_data;
+}
+let shit = downloadFile("https://raw.githubusercontent.com/vende11s/English-Project/main/database.txt");
+let shit2 = shit.text_data;
+document.getElementById("Unit-choose").innerHTML = shit2;
+console.log(shit);
+*/
+
+const database = fetch("https://raw.githubusercontent.com/vende11s/English-Project/main/database.txt")
+  .then((response) => response.text())
+  .then((data) => {
+    return data;
+  });
+
+
+  const ParseAndShow = async () => {
+	var a = await database;
+	var output = "";
+	var listOfUnits = [];
+
+
+	for(var i = 0; i < a.length-3; i++){
+		if(a[i]=="$" && a[i+1]=="$" && a[i+2]=="$"){
+
+		}
+	}
+	document.getElementById("Unit-choose").innerHTML = a;
+  };
+
+ParseAndShow();
+
+
